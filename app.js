46828192ff09a5ee1c386ad8b8f13c4b6b3d5576
AdrianSLopez <41001.adrian@gmail.com>
2023-03-23 12:50:18 -0700

    Filter and display chosen track's information

--- app.js
+++ app.js
@@ -1,53 +1,65 @@
 const prompts = require('prompts')
 const api = require('./api.js');
 
-// filter data for each track
+// filter data for each recommended track
 const _filterRecTrackInfo = (tracks) => {
     return tracks.map((track) => {
         const id = track.data.id;
         const artists = track.data.artists.items.map((artist) => {
             return artist.profile.name
         }).join(", ");
-        const title = `${track.data.name} by ${artists}`;
+        const name = `${track.data.name} by ${artists}`;
 
-        return {id, track: title}
+        return {id, name}
     });
 };
 
+const _filterTrackInfo = (track) => {
+    return track.map((trackInfo) => {
+        const albumInfo = `Track ${trackInfo.track_number} from album titled ${trackInfo.album.name}`;
+        const releaseDate = `Released in ${trackInfo.album.release_date}`
+        const time = `${((trackInfo.duration_ms)/60000).toFixed(2)} minutes long`
+        return {albumInfo, releaseDate, time: time.replace('.', ':')};
+    });
+};
+
+// display list of tracks related to user search
 const _displayTracks = async (tracks) => {
     const displayTracks = tracks.map((track) => {
-        return { title: `${track.track}`, value:track};
-    })
+        return { title: `${track.name}`, value:track};
+    });
     
     return await prompts([{
         type: 'select',
         name: 'tracks',
         message: 'select track to view more informatoin',
         choices: displayTracks
-    }])
-}
-
+    }]);
+};
 
 const get = async (trackName) => {
-    console.log(`Fetching 10 potential track`);
+    // api requests a list of 10 tracks based on user search
     const tracks = await api.getTracks(trackName);
 
-    //flters info for every recommended track
+    // filters info for every recommended track
     const filteredRecTrackInfo = _filterRecTrackInfo(tracks);
 
+    // track chosen by user
     const chosenTrack = await _displayTracks(filteredRecTrackInfo);
+    // api requests more info based on chosen track.
+    const trackInfo = await api.getTrackInfo(chosenTrack.tracks.id);
 
-    const trackInfo = await api.getTrackInfo(chosenTrack.tracks);
+    // filter chosen track info
+    const filteredTrackInfo =_filterTrackInfo(trackInfo.data.tracks);
+   
+    // Display track information
+    console.log('\t\x1b[32m%s\x1b[0m', chosenTrack.tracks.name);
+    console.log('\t\x1b[32m%s\x1b[0m', filteredTrackInfo[0].albumInfo);
+    console.log('\t\x1b[32m%s\x1b[0m', filteredTrackInfo[0].releaseDate);
+    console.log('\t\x1b[32m%s\x1b[0m', filteredTrackInfo[0].time);
 
-    // Todo: 
-        // Display Track Info:
-            // Ex:
-                // [Track title] by [Artist(s)]
-                // Track [track_number] From [Album title]
-                // Released in [release_date]
-                // 
-
-        // implement [option] for user to choose the maximum amount of reccomendations
+    // TODO:
+        // (optional) implement [option] for user to choose the maximum amount of reccomendations
 }
 
 module.exports = {
