5691c46bfdf4de25d1ea2275e52a2d2982ea814f
AdrianSLopez <41001.adrian@gmail.com>
2023-03-23 12:11:44 -0700

    Use prompts to display list of rec tracks to user.

--- app.js
+++ app.js
@@ -1,29 +1,51 @@
+const prompts = require('prompts')
 const api = require('./api.js');
 
 // filter data for each track
-const _filterTrackInfo = (tracks) => {
-    return tracks.tracks.map((track) => {
-        const trackId = track.data.id;
+const _filterRecTrackInfo = (tracks) => {
+    return tracks.map((track) => {
+        const id = track.data.id;
         const artists = track.data.artists.items.map((artist) => {
             return artist.profile.name
         }).join(", ");
         const title = `${track.data.name} by ${artists}`;
 
-        return {trackId, track: title}
+        return {id, track: title}
     });
 };
+
+const _displayTracks = async (tracks) => {
+    const displayTracks = tracks.map((track) => {
+        return { title: `${track.track}`, value:track};
+    })
+    
+    return await prompts([{
+        type: 'select',
+        name: 'tracks',
+        message: 'select track to view more informatoin',
+        choices: displayTracks
+    }])
+}
+
 
 const get = async (trackName) => {
     console.log(`Fetching 10 potential track`);
     const tracks = await api.getTracks(trackName);
 
-    //flters info for every track
-    const filteredTrackInfo = _filterTrackInfo(tracks)
+    //flters info for every recommended track
+    const filteredRecTrackInfo = _filterRecTrackInfo(tracks);
 
-    console.log(filteredTrackInfo)
+    const chosenTrack = await _displayTracks(filteredRecTrackInfo);
+
+    const trackInfo = await api.getTrackInfo(chosenTrack.tracks);
+
     // Todo: 
-        // display tracks to user with option to choose a track
-        // On track selection, display more track information by makeing another api call 
+        // Display Track Info:
+            // Ex:
+                // [Track title] by [Artist(s)]
+                // Track [track_number] From [Album title]
+                // Released in [release_date]
+                // 
 
         // implement [option] for user to choose the maximum amount of reccomendations
 }
