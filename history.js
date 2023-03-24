464c2e4c2a2d6fb8fbe55917f7841a44aeba745f
Nep2un <jupi2erinspace@gmail.com>
2023-03-24 14:49:23 -0700

    update saveSearch function

--- history.js
+++ history.js
@@ -2,19 +2,16 @@ const fs = require('fs/promises');
 
 // Save user searches to local file: history.json
 const saveSearch = async (search, resultCount) => {
-
-    await fs.readFile('history.json')
-    .then((data) => {
+    try {
+        const data = await fs.readFile('history.json');
         const obj = JSON.parse(data);
 
         obj.push({ search, resultCount });
 
         fs.writeFile('history.json', JSON.stringify(obj));
-    })
-    .catch((error) => {
+    } catch(errror) {
         fs.writeFile('history.json', JSON.stringify( [{ search, resultCount }]));
-    })
-
+    }
 };
 
 module.exports = {
