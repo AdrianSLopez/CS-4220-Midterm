65ee7cae88269dbf630dd5257ef2ea6495fb2ff7
AdrianSLopez <41001.adrian@gmail.com>
2023-03-23 17:31:45 -0700

    Add TODO

---
+++ history.js
@@ -0,0 +1,11 @@
+// TODO:
+    //Create function to store all searches and count of results
+        // to a local JSON file
+
+    // Array of Objects
+        // Object
+            // [{search: "Blue boy", resultCount: "10"}]
+    // Hint: 
+        // https://nodejs.org/api/fs.html#fswritefilefile-data-options-callback
+
+    // Export function to be used in app.js
